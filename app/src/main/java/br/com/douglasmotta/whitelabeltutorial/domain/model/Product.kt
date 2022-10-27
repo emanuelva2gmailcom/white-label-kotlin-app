1c5900a089160f3f7a76ef33221ce923cd83aac8
Emanuel Vasconcelos Nobre <emanuelvanobre@gmail.com>
2022-10-27 17:34:23 -0300

    Video 4

--- app/src/main/java/br/com/douglasmotta/whitelabeltutorial/domain/model/Product.kt
+++ app/src/main/java/br/com/douglasmotta/whitelabeltutorial/domain/model/Product.kt
@@ -1,8 +1,15 @@
 package br.com.douglasmotta.whitelabeltutorial.domain.model
 
+import android.os.Parcelable
+import com.google.firebase.firestore.PropertyName
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Product(
     val id: String = "",
     val description: String = "",
     val price: Double = 0.0,
-    val imageUrl: String = ""
-)
+    @set:PropertyName("image_url")
+    @get:PropertyName("image_url")
+    var imageUrl: String = "" // image_url
+) : Parcelable
