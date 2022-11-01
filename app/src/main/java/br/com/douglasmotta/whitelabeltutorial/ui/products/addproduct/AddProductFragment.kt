c4545585e6816fe5239a92462293f4da18472a91
Emanuel Vasconcelos Nobre <emanuelvanobre@gmail.com>
2022-11-01 17:30:37 -0300

    Video 8

--- app/src/main/java/br/com/douglasmotta/whitelabeltutorial/ui/products/addproduct/AddProductFragment.kt
+++ app/src/main/java/br/com/douglasmotta/whitelabeltutorial/ui/products/addproduct/AddProductFragment.kt
@@ -1,19 +1,30 @@
 package br.com.douglasmotta.whitelabeltutorial.ui.products.addproduct
 
+import android.net.Uri
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import br.com.douglasmotta.whitelabeltutorial.R
 import br.com.douglasmotta.whitelabeltutorial.databinding.FragmentAddProductBinding
+import br.com.douglasmotta.whitelabeltutorial.util.CurrencyTextWatcher
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class AddProductFragment : BottomSheetDialogFragment() {
 
     private var _binding: FragmentAddProductBinding? = null
     private val binding get() = _binding!!
+
+    private var imageuri: Uri? = null
+
+    private val getContent =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+            imageuri = uri
+            binding.imageProduct.setImageURI(uri)
+        }
 
     private lateinit var viewModel: AddProductViewModel
 
@@ -23,5 +34,31 @@ class AddProductFragment : BottomSheetDialogFragment() {
     ): View? {
         _binding = FragmentAddProductBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setListeners()
+    }
+
+    private fun setListeners() {
+        binding.imageProduct.setOnClickListener {
+            chooseImage()
+        }
+
+        binding.buttonAddProduct.setOnClickListener {
+            val description = binding.inputDescription.text.toString()
+            val price = binding.inputPrice.text.toString()
+
+        }
+
+        binding.inputPrice.run {
+            addTextChangedListener(CurrencyTextWatcher(this))
+        }
+    }
+
+    private fun chooseImage() {
+        getContent.launch("image/*")
     }
 }
