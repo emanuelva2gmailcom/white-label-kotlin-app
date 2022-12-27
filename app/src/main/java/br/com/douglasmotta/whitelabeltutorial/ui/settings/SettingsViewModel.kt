75dd0b125ed987622d23fda7d6fb61c29953340e
Emanuel Vasconcelos Nobre <emanuelvanobre@gmail.com>
2022-12-27 16:23:25 -0300

    refactor: melhorando tratamento de erro da página de settings.

--- app/src/main/java/br/com/douglasmotta/whitelabeltutorial/ui/settings/SettingsViewModel.kt
+++ app/src/main/java/br/com/douglasmotta/whitelabeltutorial/ui/settings/SettingsViewModel.kt
@@ -1,8 +1,10 @@
 package br.com.douglasmotta.whitelabeltutorial.ui.settings
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import br.com.douglasmotta.whitelabeltutorial.domain.usecase.auth.SignOutUseCase
-import br.com.douglasmotta.whitelabeltutorial.ui.BaseViewModel
+import br.com.douglasmotta.whitelabeltutorial.util.Result
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -11,7 +13,15 @@ class SettingsViewModel @Inject constructor(
     private val signOutUseCase: SignOutUseCase
 ) : ViewModel() {
 
+    private val _errorResultData = MutableLiveData<Result<Boolean>>()
+    val errorResultData: LiveData<Result<Boolean>> = _errorResultData
+
     fun logout() {
-        signOutUseCase()
+        try {
+            signOutUseCase()
+            _errorResultData.value = Result.Success(true)
+        } catch (e: Exception) {
+            _errorResultData.value = Result.Error(e)
+        }
     }
 }
